5f1e8d81efaaa95c5a0378ca9e8a6efb67accd4b
Yujie Hao <haoyj20@gmail.com>
2018-11-18 18:36:38 -0800

    use readFileSync to synchronously read the file line by line

--- ecommerce_vendor_database_web_app/controllers/fileController.js
+++ ecommerce_vendor_database_web_app/controllers/fileController.js
@@ -4,23 +4,29 @@ const formidable = require('formidable');
 const fs         = require('fs');
 const path       = require('path');
 
-const fileController = {
-  saveFile : (req, res) => {
-    const form = new formidable.IncomingForm();
-    form.parse(req, (err, fields, files) => {
-      const oldpath = files.filetoupload.path;
-      console.log('oldpath: ' + oldpath);
-      const newpath = path.join(__dirname, '../files/' + files.filetoupload.name);
-      console.log('newpath: ' + newpath);
-      fs.rename(oldpath, newpath, (err) => {
-        if (err) {
-          throw err;
-        }
-        res.write('File uploaded and moved!');
-        res.end();
-      });
-    });
-  }  
+const fileController = {};
+
+fileController.parseFile = (filePath) => {
+  console.log('Parsing file: ' + filePath);
+  let i = 1;
+  fs.readFileSync(filePath).toString().split('\n').forEach((line) => {
+    console.log('line#' + i++ + ': ' + line);
+  });
+};
+
+fileController.saveFile = (req, res) => {
+  const form = new formidable.IncomingForm();
+  form.parse(req, (err, fields, files) => {
+    const oldpath = files.filetoupload.path;
+    console.log('oldpath: ' + oldpath);
+    const newpath = path.join(__dirname, '../files/' + files.filetoupload.name);
+    console.log('newpath: ' + newpath);
+    fs.renameSync(oldpath, newpath);
+    res.write('File uploaded and moved!');
+    res.end();
+    console.log('Finished saving file');
+    fileController.parseFile(newpath);
+  });
 };
 
 module.exports = fileController;
